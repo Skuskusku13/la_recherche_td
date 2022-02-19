1011c28ddcaa82873190fc6102d82b6f4641a8f4
Skuskusku13 <danlevy149@gmail.com>
2022-02-19 20:04:20 +0100

    new(navbar)

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,12 @@
 import theme from '../Theme/Theme';
 import { ChakraProvider } from '@chakra-ui/react';
+import Navbar from '../components/navbar';
+import "../css/navbar.css";
 
 function MyApp({ Component, pageProps }) {
     return (
         <ChakraProvider theme={theme}>
+            <Navbar />
             <Component {...pageProps} />
         </ChakraProvider>
     );
